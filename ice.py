cb8c52d90cec4d2a016090dfe409123c7039e336
ShitalKandel <kandelshittal@gmail.com>
2023-11-27 21:43:30 +0545

    function based

--- ice.py
+++ ice.py
@@ -23,9 +23,9 @@ flavors_prices = {
 }
 
 toppings_prices = {
-        1: 60,
-        2: 40,
-        3: 50
+        '1': 60,
+        '2': 40,
+        '3': 50
     }
 def add_flavors():
     
@@ -33,10 +33,8 @@ def add_flavors():
     if user_input not in flavors_prices:
         print("Invalid input")
         return flavors_prices.get(6,0)
-    if user_input in flavors_prices:
-        print("Valid Flavor Price")
     else:
-        print("Nothing was chosen")
+        print("Valid Flavor Price")
     selected_flavor = flavors_prices.get(user_input, 0)
     return selected_flavor
 
@@ -46,41 +44,44 @@ def add_toppings():
     total_toppings_price = 0
     while True:
         user_input = input("Select additional topping from 1-3, type 'done' when finish): ")
-        if user_input in toppings_prices:
-            print("Valid Toppings Price")
-    
-        else:
-            if user_input.lower() == 'done':
+        if user_input.lower() == 'done':
                 break
-        total_toppings_price += toppings_prices.get(int(user_input), 0)
-        # choosen_toppings.append(selected_topping)
-    return total_toppings_price
+        elif user_input in toppings_prices:
+            print("Valid Toppings Price")
+            # total_toppings_price += toppings_prices.get(int(user_input), 0)
+            total_toppings_price += toppings_prices[user_input]
+            choosen_toppings.append(user_input)#total_toppings_price
+        else:
+            print("Invalid input for toppings")
+    return total_toppings_price , choosen_toppings
 
 def calculate_total(selected_flavor, choosen_toppings):
     # ice_cream_price = selected_flavor
     # toppings_price = sum(choosen_toppings)
     # total_price = ice_cream_price + toppings_price
-    return selected_flavor + choosen_toppings
+    return selected_flavor + choosen_toppings 
 
 def print_bill(selected_flavor, choosen_toppings, total_price):
     print("----- Bill -----\n")
     print(f"Selected Ice Cream: Rs.{selected_flavor:.2f}")
     print("Selected Toppings:")
-
-
+    # for tooping in choosen_toppings:
+        # print(f"{toppings_prices}")
     print(f"Total Price: Rs.{total_price:.2f}")
 
-
+#add date time
     with open("ice_cream_bill.txt", "a") as file:
         file.write(f"Selected Ice Cream: Rs.{selected_flavor:.2f}\n")
         file.write("Selected Toppings:\n")
+        for topping in choosen_toppings:
+            file.write(f"{toppings_prices}")
         
         file.write(f"Total Price: Rs.{total_price:.2f}\n\n")
 
 def get_fun():
-    flavor = int(input("Choosen flavor : "))
+    flavor = int(input("Selected flavor : "))
     while flavor not in flavors_prices:
-        flavor = int(input("Pleased to re-order again : "))
+        flavor = int(input("Please re-order the flavor : "))
     return flavors_prices.get(flavor)
         
         
@@ -90,8 +91,9 @@ def main():
     flavor_price = get_fun()
     print(flavor_price)
     extra_topping()
-    choosen_toppings = add_toppings()  
-    total_price = calculate_total(flavor_price, choosen_toppings)  
+    total_toppings_price,choosen_toppings = add_toppings()  
+    print(total_toppings_price)#change choosen toopings into total_toopings_price
+    total_price = calculate_total(flavor_price, total_toppings_price)  
     print(total_price)
     print_bill(flavor_price, choosen_toppings, total_price)
     
