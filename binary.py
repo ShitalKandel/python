15ee5c492f8dd2dbe5f01949e7073feca90122c8
Mozarcx Iaye <mozarcxiaye@gmail.com>
2023-11-30 22:24:26 +0545

    Corrected binary.py

--- binary.py
+++ binary.py
@@ -4,15 +4,15 @@ def binary_search(my_num,target):
     n = len(my_num)
 
     while i < n:
-        m = (i + n)//2
+        m = i + (n - i) // 2
 
         if my_num[m] == target:
-            return my_num[m]
+            return m
         elif my_num[m] <= target:
-            n = m + 1
+            i = m + 1
 
         else:
-            i = m + 1
+            n = m - 1
 
 my_num = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 target = 6
