77c94cafbfa1cb3402b9938c2e0973b7df15ce8a
ShitalKandel <kandelshittal@gmail.com>
2023-12-03 17:51:14 +0545

    New Commit

--- hello.py
+++ hello.py
@@ -1,22 +1,19 @@
 import csv
 import os
+from datetime import datetime
 
 # Main Class
 class CSV_Saver:
-        
-    
-    def write_to_csv(cls, data, csv_file, header=True):
+    def write_to_csv(self, data, csv_file, header=True):
         with open(csv_file, 'w', newline='') as file:
             csv_writer = csv.writer(file)
-            
             if header:
                 csv_writer.writerow(data[0])
                 csv_writer.writerows(data[1:])
             else:
                 csv_writer.writerows(data)
 
-    
-    def read_to_csv(cls, csv_file):
+    def read_to_csv(self, csv_file):
         rows = []
         with open(csv_file, 'r') as file:
             csvreader = csv.reader(file)
@@ -24,85 +21,70 @@ class CSV_Saver:
                 rows.append(row)
         return rows
 
-    
-    def delete_to_csv(cls, csv_file):
+    def delete_to_csv(self, csv_file):
         if os.path.exists(csv_file):
             os.remove(csv_file)
             print(f"File {csv_file} deleted.")
         else:
             print(f"File {csv_file} not found.")
 
-    def update_to_csv(cls, csv_file):
-        data = cls.read_to_csv(csv_file)
-        
-        # Check if there is at least one row in the CSV file before updating
-        if len(data) > 0:
-            data[0][3] = "Updated Data"  
-            cls.write_to_csv(data, csv_file)
+    def update_to_csv(self, csv_file):
+        data = self.read_to_csv(csv_file)
+        if len(data) > 0 and len(data[0])>3 :
+            data[0][3] = "Updated Data"
+            self.write_to_csv(data, csv_file)
         else:
             print(f"CSV file {csv_file} is empty. Nothing to update.")
 
     @staticmethod
-    def create_csv_file(csv_file, header):
-        with open(csv_file, 'w', newline='') as file:
-            csv_writer = csv.writer(file)
-            csv_writer.writerow(header)
+    def create_csv_file(data, csv_file, header):
+        CSV_Saver().write_to_csv(data, csv_file, header)
 
 
 # Child Class
 class CSV_Operation(CSV_Saver):
-    
-
-    @classmethod
-    def __init__(self, data, csv_file, header):
-        super().__init__()
+    def __init__(self, csv_file, header):
         self.csv_file = csv_file
-        self.data = data
         self.header = header
-        
-    
-    def write_to_csv(self):
-        super().write_to_csv(self.data, self.csv_file)
+        self.operation_log = []  # To keep a record of CSV operations with time
 
-    def create_csv_file(self):
-        super().create_csv_file(self.csv_file, self.header)
+    def create_csv_file(self, data):
+        # Record the timestamp of the operation
+        timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        operation = f"Create CSV File: {self.csv_file} at {timestamp}"
+        self.operation_log.append(operation)
 
-    def read_to_csv(self):
-        return super().read_to_csv(self.csv_file)
+        # Call the method from the parent class to write to the CSV file
+        self.write_to_csv(data, self.csv_file, self.header)
 
-    def delete_to_csv(self):
-        super().delete_to_csv(self.csv_file)
+    def update_csv_file(self):
+        # Record the timestamp of the operation
+        timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        operation = f"Update CSV File: {self.csv_file} at {timestamp}"
+        self.operation_log.append(operation)
 
-    def update_to_csv(self):
+        # Call the method from the parent class to update the CSV file
         super().update_to_csv(self.csv_file)
 
+    def get_operation_log(self):
+        return self.operation_log
 
+
+# Sample data for testing
 data = [
-    ["Name", "Age", "City", "Skill"],
-    ["John Wick", 25, "Lalitpur", "Actor"],
-    ["Roman Range", 30, "Kathmandu", "Fake Wrestler"],
-    ["Bob Marley", 22, "Bhaktapur", "Artist"]
+    ["Name", "Age", "City"],
+    ["John Wick", 25, "Lalitpur"],
+    ["Roman Range", 30, "Kathmandu"],
+    ["Bob Marley", 22, "Bhaktapur"]
 ]
 
+# Create an instance of the CSV_Operation class with CSV file name and header
+csv.obj = CSV_Operation("python.csv", ["Name", "Age", "City"])
 
-obj = CSV_Operation().create_csv_file(data,"python.csv", ["Name", "Age", "City", "Skills"])
-# obj.create_csv_file("Name", "Age", "City","Skill")
-# obj.write_to_csv()
-# obj.read_to_csv()
-# obj.update_to_csv()
-# obj.delete_to_csv()
+# Create CSV file and update it
+csv.obj.create_csv_file(data)
+csv.obj.update_csv_file()
 
-# obj =CSV_Operation(data, "python.csv", ["Name", "Age", "City", "Skills"])
-# obj.write_to_csv()
-
-# CSV_Operation(data, "python.csv", ["Name", "Age", "City", "Skills"]).write_to_csv()
-
-# obj.write_to_csv()
-
-# CSV_Operation.write_to_csv()
-# print(CSV_Operation(data, "python.csv", ["Name", "Age", "City", "Skills"]).write_to_csv())
-
-# CSV_Operation.update_to_csv()
-# CSV_Operation.delete_to_csv()
-
-
+# Print the operation log
+print("Operation Log:")
+print(csv.obj.get_operation_log())
